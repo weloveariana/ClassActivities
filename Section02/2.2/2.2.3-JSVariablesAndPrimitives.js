7376a59e16a6de4281963db60622f3b05ca583d9
Ariana <ariana.philips024@gmail.com>
2024-07-08 16:20:25 -0500

    JS Variables and Primitives 2.2.3

---
+++ Section02/2.2/2.2.3-JSVariablesAndPrimitives.js
@@ -0,0 +1,39 @@
+// Create a variable "timeOfDay" and assign it a string
+timeOfDay = "Noon";
+// Create a variable "greeting" that references a template literal
+// Use "timeOfDay" in the template literal to create a message like "Good morning!" or "Good evening!"
+let greeting = "Good Morning!";
+
+// Print "greeting"
+
+console.log(greeting);
+
+// Create a new variable, but do not assign it a value
+
+// Print the new variable and its type
+// What type should we expect?
+
+// let dayofWeek "Monday";
+// console.log(dayofWeek); // My guess is for this to show as undefined since it has not been assigned.
+// When I tried to run code there was an error. I decided to comment out this section to test out the rest of thee code.
+
+// Assign the variable a value that indicates the variable is purposely blank
+// What value should we assign?
+
+let dayWeek = "Monday";
+console.log(typeof dayWeek);
+console.log(dayWeek);
+
+// Print the variable and its type again
+// What type should we expect? *To print a string and also show the value of the variable we set which is Monday.
+
+// Try to print a variable that does not exist
+// What should we expect to print in the CLI?
+
+console.log(name); // This showed up as undefined.
+
+// Print "greeting" again
+// Will this line run? *This line will NOT run because the previous line is not functionable because it has not been defined within the code.
+// If I were to comment this section out like I did the previous one, I would be able to run the last greeting line.
+
+console.log(greeting);
